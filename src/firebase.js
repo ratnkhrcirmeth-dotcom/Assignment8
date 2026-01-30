edeabae1d1c9fa6e82e7b76c9c8ad540afd52676
coach <sompastell@gmail.com>
2026-01-30 08:37:14 +0700

    update firebase and css

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
-import { getFirestore } from "firebase/firestore"; 
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2TZ-KbnbWxhEtNdiBlSanz4fU2a0ef4Y",
-  authDomain: "test20-1-69.firebaseapp.com",
-  projectId: "test20-1-69",
-  storageBucket: "test20-1-69.firebasestorage.app",
-  messagingSenderId: "598747664727",
-  appId: "1:598747664727:web:21a721ac3acee8a3cbae12",
-  measurementId: "G-H2Y1R9SNNF"
+  apiKey: "AIzaSyAI13O_JzJagcGtF89DIt86zpewjoDjkmw",
+  authDomain: "jiramet-web-50632.firebaseapp.com",
+  projectId: "jiramet-web-50632",
+  storageBucket: "jiramet-web-50632.firebasestorage.app",
+  messagingSenderId: "97607325138",
+  appId: "1:97607325138:web:ee281061f2e8aa9e4a240b",
+  measurementId: "G-GCFTC82PPT"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
